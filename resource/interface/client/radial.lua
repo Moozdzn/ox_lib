a2588a752d3f2c219f594da8a53a4b4bd19b4a23
TheImp1000 <moozdzn@gmail.com>
2023-03-01 09:42:16 +0000

    Change radial to toggle instead of hold

--- resource/interface/client/radial.lua
+++ resource/interface/client/radial.lua
@@ -315,7 +315,7 @@ lib.addKeybind({
             Wait(0)
         end
     end,
-    onReleased = lib.hideRadial,
+    --onReleased = lib.hideRadial,
 })
 
 AddEventHandler('onClientResourceStop', function(resource)
