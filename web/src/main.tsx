9efceb0d17af7a713280a355ea567e811f892cbb
TheImp1000 <moozdzn@gmail.com>
2023-03-01 23:17:17 +0000

    fontawesome pro

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/pro-solid-svg-icons';
+import { far } from '@fortawesome/pro-regular-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fal } from '@fortawesome/pro-light-svg-icons';
+import { fat } from '@fortawesome/pro-thin-svg-icons';
+import { fad } from '@fortawesome/pro-duotone-svg-icons';
+import { fass } from '@fortawesome/sharp-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { isEnvBrowser } from './utils/misc';
 import LocaleProvider from './providers/LocaleProvider';
 import ConfigProvider from './providers/ConfigProvider';
 
-library.add(fas, far, fab);
+library.add(fas, far, fab, fal, fat, fad, fass);
 
 if (isEnvBrowser()) {
   const root = document.getElementById('root');
